dac661026230332ef261ff229e5e3353e8cfd8b6
millandavid <millanium28@gmail.com>
2023-02-25 19:05:18 +0000

    Added goose counter

--- game/gui/scorehud.lua
+++ game/gui/scorehud.lua
@@ -12,6 +12,7 @@ function scorehud:init()
     self.defaultfont = love.graphics.getFont()
     self.hudfont = self:loadfont("assets/joystix.ttf", 40)
     self.hudtexture = love.graphics.newImage("assets/hud.png")
+    self.hud_hit_goose = love.graphics.newImage("assets/hit-goose.png")
 end
 
 function scorehud:draw(bulletCount, gooseCount, score)
@@ -19,6 +20,20 @@ function scorehud:draw(bulletCount, gooseCount, score)
     love.graphics.draw(self.hudtexture, 0, 0)
     love.graphics.print("000000", self.hudfont, 0, 0)
     love.graphics.setFont(self.defaultfont)
+   
+  end
+  
+
+function scorehud:goose_counter()
+    goose_x = 430
+    goose_y = 560
+    goose_x_change = 45
+
+    for i = 0, 9 do
+      love.graphics.draw(self.hud_hit_goose, goose_x + i * goose_x_change, goose_y)
+    
+    -- love.graphics.draw(self.hud_hit_goose, goose_x, goose_y)
+    end
 end
 
 return scorehud
