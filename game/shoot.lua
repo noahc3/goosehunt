61bf0177415f066c2fef33d72adad06b07fcfba3
mbattad <mbattad@me.com>
2023-02-25 23:42:17 +0000

    adding goose collision detection w/cursor

--- game/shoot.lua
+++ game/shoot.lua
@@ -1,8 +1,29 @@
 module("shoot", package.seeall)
 
+goose_module = require "goose"
+
 -- functions for shooting shit yeehaw
 
-function shoot:shoot(joystick, coords)
+CURSORSIZE = 30
+
+function centrecoords(coords)
+    return {coords[1] + (CURSORSIZE/2), coords[2] + (CURSORSIZE/2)}
+end
+
+function collides(coords, goose)
+    return (goose.x <= coords[1]) and (goose.x + goose.width >= coords[1]) and (goose.y <= coords[2]) and (coords[2] <= goose.y + goose.height)
+end
+
+
+
+function shoot:shoot(joystick, coords, geeselist)
     -- apply max rumble for 0.5 seconds
     local vibe = joystick:setVibration(1, 1, 0.5)
+    centredcoords = centrecoords(coords)
+
+    for i,goose in ipairs(geeselist) do
+        if collides(centredcoords, goose) then
+            goose:get_shot()
+        end
+    end
 end
