a214e4f4ba43f73517203f7db8e252860bd4ee35
Alex Kitt <alex.kitt@outlook.com>
2023-02-25 18:57:31 +0000

    Beginnings of state

--- game/goose.lua
+++ game/goose.lua
@@ -14,18 +14,26 @@ function Goose:new(x, y, width, height)
     new_goose.height = height
     new_goose.velocity_x = 60
     new_goose.velocity_y = -120
+    new_goose.states = {FLYING = 0, SHOT = 1, FALLING = 2}
+    new_goose.state = new_goose.states.FLYING
 
     return new_goose
 end
 
 function Goose:update(dt)
-    self.x = self.x + self.velocity_x * dt
-    self.y = self.y + self.velocity_y * dt
+    if self.state == self.states.FLYING then
+        self.x = self.x + self.velocity_x * dt
+        self.y = self.y + self.velocity_y * dt
+    end
 end
 
 function Goose:draw()
     self.cool = 1/0
     love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
+end
+
+function Goose:get_shot()
+
 end
 
 return Goose
